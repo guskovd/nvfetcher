3b83821533f378e6461e85eefa4f901498a7dfc6
Danil Guskov <guskovd86@mail.ru>
2026-04-27 17:21:28 +0300

    update

--- src/NvFetcher/NixFetcher.hs
+++ src/NvFetcher/NixFetcher.hs
@@ -203,7 +203,7 @@ gitHubFetcher ::
   -- | owner and repo
   (Text, Text) ->
   PackageFetcher
-gitHubFetcher (owner, repo) rev = FetchGitHub owner repo rev False False False False False [] Nothing ()
+gitHubFetcher (owner, repo) rev = FetchGitHub owner repo rev False False False True False [] Nothing ()
 
 -- | Create a fetcher from pypi
 pypiFetcher :: Text -> PackageFetcher
